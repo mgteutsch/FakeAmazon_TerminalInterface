6f61c70a3cb575e5866d56afa17b91764bff8850
mgteutsch <mgt0117@gmail.com>
2017-04-05 14:46:30 -0700

    Initial commit, basic menu established

---
+++ FakeAmazon_TerminalInterface/AccountOptions/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeAmazon_TerminalInterface.AccountOptions
+{
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+        public int Phone { get; set; }
+
+        public void CreateNewCustomer()
+        {
+            Console.WriteLine("Let's start with your name. Enter your first name below and press Enter:");
+            string FirstName = Console.ReadLine();
+
+        }
+    }
+}
---
+++ FakeAmazon_TerminalInterface/Program.cs
@@ -0,0 +1,50 @@
+﻿using FakeAmazon_TerminalInterface.AccountOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeAmazon_TerminalInterface
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("**  Welcome to Bangazon! Command Line Ordering System  **");
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2. Create a payment option");
+            Console.WriteLine("3. Order a product");
+            Console.WriteLine("4. Complete an order");
+            Console.WriteLine("5. See product popularity");
+            Console.WriteLine("6. Leave Bangazon!");
+
+            Customer customer = new Customer();
+
+            while (isRunning)
+            {
+                string userSelection = Console.ReadLine();
+
+                if (userSelection == "1")
+                {
+                    Console.WriteLine("Great! Let's get you signed up!");
+                    customer.CreateNewCustomer();
+                }
+                else if (userSelection == "6")
+                {
+                    Console.WriteLine("Thanks for visiting FakeAmazon! Have a nice day!");
+                    isRunning = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry, that option is not valid. Please choose and option 1 through 6.");
+                }
+            }
+        }
+    }
+}
