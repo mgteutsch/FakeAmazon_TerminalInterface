327038ad41f014b8bd98e1f7c34e72a4b84bd1ab
mgteutsch <mgt0117@gmail.com>
2017-04-18 00:00:33 -0500

    Basic payment method structure built -- finish Repository

---
+++ FakeAmazon_TerminalInterface/Controllers/Contracts/IPayment.cs
@@ -0,0 +1,18 @@
+﻿using FakeAmazon_TerminalInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeAmazon_TerminalInterface.Controllers.Contracts
+{
+    interface IPayment
+    {
+        void AddPaymentType(string name, int acctNum, int customerId);
+
+        Customer GetCustomer(int customerId);
+
+        List<Payment> GetPaymentList(int customerId);
+    }
+}
